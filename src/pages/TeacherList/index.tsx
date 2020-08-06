3cfe04017084ad90912213f301bcb47914f020ce
Caio Poli <caio.poli@ernetwork.com.br>
2020-08-05 23:10:15 -0300

    :building_construction: Build PageHeader component

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -6,7 +6,24 @@ import PageHeader from '../../components/PageHeader';
 function TeacherList() {
   return(
     <div id="page-teacher-list" className="container">
-      <PageHeader />
+      <PageHeader title="Estes são os proffys disponiveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matérias</label>
+            <input type="text" id="subject"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da Semana</label>
+            <input type="text" id="subject"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time"/>
+          </div>
+        </form>
+      </PageHeader>
     </div>
   )
 }
